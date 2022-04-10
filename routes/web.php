e79d194fe6a5600e06109a3a1a905dead9fc992d
prasoonsaini <prasoonsaini2002@gmail.com>
2022-04-10 09:22:05 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+
+Route::get('/',[HomeController::class, 'show_post'])->name('home');
+
+
+Route::middleware(['auth'])->group(function(){
+Route::get('/dashboard',[DashboardController::class,'show_post'])->name('dashboard');
+Route::get('/post',[PostController::class,'index'])->name('post_index');
+Route::post('/post',[PostController::class,'create'])->name('post_create');
+Route::get('/post/edit/{id}',[PostController::class,'edit'])->name('post_edit');
+Route::post('/post/edit/{id}',[PostController::class,'update'])->name('post_update');
+Route::get('/post/delete/{id}',[PostController::class,'destroy'])->name('post_destroy');
+
+});
+
+require __DIR__.'/auth.php';
