494b7cd491cd122ab94009dc14e158a66ce89ce7
prasoonsaini <prasoonsaini2002@gmail.com>
2022-04-10 10:25:58 +0530

    Dashboard column updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -54,7 +54,7 @@
                                                 <th scope="col" class="px-6
                                 py-3 text-left text-xs 
                                 font-medium text-gray-100
-                                uppercase tracking-wider">Body
+                                uppercase tracking-wider">Symptoms
                                                 </th>
                                                 <th scope="col" class="px-6
                                 py-3 text-left text-xs 
